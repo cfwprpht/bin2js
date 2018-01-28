dd8c81b9a6b5e270ff773e2d72999acb01e857c9
cfwprpht <darknight3111@gmx.at>
2018-01-28 08:29:47 +0100

    Added a thrid format and back convertion.
    Added Vortex Payload Foramt and back Convertion.

--- ReadMe.md
+++ ReadMe.md
@@ -7,3 +7,11 @@ unsigned integer 32 Array, embeded into a JavaScript function call.
 The application is ready to use for the PS4 OpenSource Hacking Community.
 
 Added a second format for the .js to write. Simple use any flag to trigger it.
+Added Vortex Payload Format.
+Added back convertion supportfor all 3 formats.
+
+Flags:
+-1 = Foramt1 u32[]
+-2 = Format2 p.write4addr.add(...
+-3 = Format3 payload = [....
+-4 = js2bin
--- bin2js/Program.cs
+++ bin2js/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Runtime.InteropServices;
@@ -27,6 +28,14 @@ namespace bin2js {
         public static bool Contains(this string source, string toCheck, StringComparison comparison) { return source.IndexOf(toCheck, comparison) >= 0; }
 
         /// <summary>
+        /// Checks if a string do contain a specific string including StringCoparison option. (Auto ignoreCase)
+        /// </summary>
+        /// <param name="source">The source string to check.</param>
+        /// <param name="toCheck">The string that shall be looked for.</param>
+        /// <returns>True if the string to look for was found., else false.</returns>
+        public static bool Contain(this string source, string toCheck) { return source.IndexOf(toCheck, StringComparison.InvariantCultureIgnoreCase) >= 0; }
+
+        /// <summary>
         /// Write a string to a file.
         /// </summary>
         /// <param name="destination">The file to write into.</param>
@@ -38,6 +47,40 @@ namespace bin2js {
                     using (TextWriter tw = sw) {
                         if (args != null) tw.WriteLine(message, args);
                         else tw.WriteLine(message);
+                        tw.Close();
+                    }
+                }
+            } catch (IOException io) { throw new Exception(io.ToString()); }
+        }
+
+        /// <summary>
+        /// Write a string to a file.
+        /// </summary>
+        /// <param name="destination">The file to write into.</param>
+        /// <param name="message">The message to write.</param>
+        public static void WriteAllLines(this string destination, string[] message) {
+            try {
+                using (StreamWriter sw = File.AppendText(destination)) {
+                    using (TextWriter tw = sw) {
+                        foreach (string line in message) tw.WriteLine(line);
+                        tw.Close();
+                    }
+                }
+            } catch (IOException io) { throw new Exception(io.ToString()); }
+        }
+
+        /// <summary>
+        /// Write a string to a file.
+        /// </summary>
+        /// <param name="destination">The file to write into.</param>
+        /// <param name="message">The message to write.</param>
+        /// <param name="args">The arguments that shall be swapped with the place holders.</param>
+        public static void Write(this string destination, string message, [Optional] params object[] args) {
+            try {
+                using (StreamWriter sw = File.AppendText(destination)) {
+                    using (TextWriter tw = sw) {
+                        if (args != null) tw.Write(message, args);
+                        else tw.Write(message);
                         tw.Close();
                     }
                 }
@@ -95,6 +138,15 @@ namespace bin2js {
             } catch (IOException io) { throw new Exception(io.ToString()); }
             return source;                                                                                                   // Return the result.
         }
+
+        /// <summary>
+        /// XReg Replace some pattern from a source string with a given string.
+        /// </summary>
+        /// <param name="source">The source string to use.</param>
+        /// <param name="searchPattern">The pattern to search for.</param>
+        /// <param name="replaceWith">The string to replace with.</param>
+        /// <returns>The Replaced string, if any found.</returns>
+        public static string XReplace(this string source, string searchPattern, string replaceWith) { return Regex.Replace(source, searchPattern, replaceWith); }
     }
 
     /// <summary>
@@ -127,8 +179,6 @@ namespace bin2js {
         /// </summary>
         private const string newCrap = "    p.write4(addr.add32(0x{0}), 0x{1});";
 
-        private static bool flag = false;
-
         /// <summary>
         /// Reverse a Hexstring BigEndian wise.
         /// </summary>
@@ -144,17 +194,43 @@ namespace bin2js {
         }
 
         /// <summary>
+        /// Convert a HexString to a byte Array.
+        /// </summary>
+        /// <returns>The byted HexString.</returns>
+        public static byte[] HexStringToByte(string hexString) {
+            byte[] bytedString = new byte[hexString.Length / 2];
+            for (int i = 0; i < hexString.Length / 2; i++) bytedString[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return bytedString;
+        }
+
+        /// <summary>
         /// Check arguments.
         /// </summary>
         /// <param name="args">The arguments to check.</param>
         private static void CheckArgs(string[] args) {
             if (args.Length == 0 || args.Length > 2) ShowUsage();
-            if (args.Length == 2) flag= true;
+            if (args.Length == 2) {
+                if (args[1] != "-1") {
+                    if (args[1] != "-2") {
+                        if (args[1] != "-3") {
+                            if (args[1] != "-4") {
+                                Console.WriteLine("wrong arguments !");
+                                ShowUsage();
+                                Console.WriteLine("\nPress any key to continue...");
+                                Console.ReadLine();
+                                Environment.Exit(0);
+                            }
+                        }
+                    }
+                }
+            }
             if (!args[0].Contains(".bin", StringComparison.InvariantCultureIgnoreCase)) {
-                Console.WriteLine("Please feed me with a binary !");
-                Console.WriteLine("\nPress any key to continue...");
-                Console.ReadLine();
-                Environment.Exit(0);
+                if (!args[0].Contains(".js", StringComparison.InvariantCultureIgnoreCase)) {
+                    Console.WriteLine("Please feed me with a binary or a java script file !");
+                    Console.WriteLine("\nPress any key to continue...");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
             }
         }
 
@@ -163,7 +239,7 @@ namespace bin2js {
         /// </summary>
         private static void ShowUsage() {
             Console.WriteLine("Wrong Input !");
-            Console.WriteLine("Usage: bin2js.exe <input.bin>");
+            Console.WriteLine("Usage: bin2js.exe <input.bin> <options>\n  -2  = Format2\n  -3  = Format3\n  -4  = js2bin\n");
             Console.WriteLine("\nPress any key to continue...");
             Console.ReadLine();
             Environment.Exit(0);
@@ -193,31 +269,70 @@ namespace bin2js {
             if (File.Exists(fi.FullName)) {                                                                                           // If file exists.
                 ASCIIEncoding encode = new ASCIIEncoding();                                                                           // Encoder instance.
                 try {
-                    byte[] file = ReadIntoBuffer(args[0], (int)fi.Length);                                                            // Read file into buffer.
-                    string newFile = (args[0].GetPath() + args[0].GetName().Replace(".bin", ".js")).TestFileName();                   // Get the new file name.
-                    File.Create(newFile).Close();                                                                                     // Create output file and check name availability.
-                    newFile.WriteLine("// https:" + "//github.com/cfwprpht/bin2js\n");                                                // Write some repo info into the file.
-                    byte[] trick = encode.GetBytes("function write" + newFile.GetName().Replace(".js", "") + "(write) {\n  setBase(write);\n"); // Encode the function string to bytes.
-                    byte[] trick2 = encode.GetBytes("function payload(p, addr) {\n");                                                 // Encode the function string to bytes.
-                    if (!flag) trick.Write(newFile);                                                                                  // Write the Function and name it like the input bin.
-                    else trick2.Write(newFile);
-
-                    using (BinaryReader br = new BinaryReader(new MemoryStream(file))) {                                              // Initialize a binary reader and point him to the file buffer.
-                        byte[] toStringify = new byte[4];                                                                             // Initialize a byte array to read the converting bytes into.
-                        int index, readed;                                                                                            // Initialize some integer counters.
-                        index = readed = 0;                                                                                           // Set them to 0.
-                        while (readed != file.Length) {                                                                               // Loop over all bytes now and convert.
-                            toStringify = br.ReadBytes(toStringify.Length);                                                           // Read 4 bytes.
-
-                            if (!flag) newFile.WriteLine(u32, index.ToString(), BitConverter.ToString(toStringify).Replace("-", "")); // Stringify the bytes, format and write it into the file.
-                            else newFile.WriteLine(newCrap, EndianSwapp(BitConverter.ToString(BitConverter.GetBytes(index * 4)).Replace("-", "")), BitConverter.ToString(toStringify).Replace("-", ""));   // Stringify the bytes, format and write it into the file.
-
-                            index++;                                                                                                  // Count indexer up.
-                            readed += toStringify.Length;                                                                             // Count readed bytes up.
+                    if (args[1] == "-4") {
+                        string newFile = (args[0].GetPath() + args[0].GetName().Replace(".js", ".bin")).TestFileName();               // Get the new file name.
+                        List<string> newBin = new List<string>();
+                        foreach (string line in File.ReadAllLines(args[0])) {
+                            if (line.Contain("  u32[")) newBin.Add(line.Replace("[", "").Replace("]", "").XReplace(@"  u32(\d+) = 0x", "").Replace(";", ""));
+                            else if (line.Contain("    p.write4(addr.add32(0x")) newBin.Add(line.Replace("[", "").Replace("]", "").Replace("(", "").Replace(")", "").Replace(".", "").XReplace("    pwrite4addradd320x([a-zA-Z0-9]{8}), 0x", "").Replace(";", ""));
+                            else if (line.Contain("payload = [")) {
+                                string[] splitted = line.Replace("payload = [", "").Replace("]", "").Split(new char[] { ',' });
+                                string correct = string.Empty;
+                                foreach (string _line in splitted) {
+                                    correct = string.Empty;
+                                    if (_line.Length < 8) {                                        
+                                        for (int i = _line.Length; i < 8; i++) correct += "0";
+                                        correct += _line;
+                                    } else correct = _line;
+                                    newBin.Add(correct);
+                                } break;
+                            }
                         }
-                    } // Write the closing clamb for the function.
-                    trick = encode.GetBytes("}\n");                                                                                   // Encode the function closing clamb.
-                    trick.Write(newFile);
+
+                        using (BinaryWriter binWriter = new BinaryWriter(new FileStream(newFile, FileMode.Create, FileAccess.Write))) {
+                            foreach (string toConvert in newBin.ToArray()) {
+                                byte[] converted = new byte[4];
+                                converted = HexStringToByte(toConvert);
+                                binWriter.Write(converted);
+                            }
+                            binWriter.Close();
+                        }
+                    } else {
+                        byte[] file = ReadIntoBuffer(args[0], (int)fi.Length);                                                            // Read file into buffer.
+                        string newFile = (args[0].GetPath() + args[0].GetName().Replace(".bin", ".js")).TestFileName();                   // Get the new file name.
+                        File.Create(newFile).Close();                                                                                     // Create output file and check name availability.
+                        newFile.WriteLine("// https:" + "//github.com/cfwprpht/bin2js\n");                                                // Write some repo info into the file.
+                        byte[] trick = encode.GetBytes("function write" + newFile.GetName().Replace(".js", "") + "(write) {\n  setBase(write);\n"); // Encode the function string to bytes.
+                        byte[] trick2 = encode.GetBytes("function payload(p, addr) {\n");                                                 // Encode the function string to bytes.
+                        byte[] trick3 = encode.GetBytes("payload = [");
+                        if (args[1] == "-2") trick2.Write(newFile);                                                                       // Write the Function and name it like the input bin.
+                        else if (args[1] == "-3") trick3.Write(newFile);
+                        else if (args[1] == "-1") trick.Write(newFile);
+
+                        using (BinaryReader br = new BinaryReader(new MemoryStream(file))) {                                              // Initialize a binary reader and point him to the file buffer.
+                            byte[] toStringify = new byte[4];                                                                             // Initialize a byte array to read the converting bytes into.
+                            int index, readed;                                                                                            // Initialize some integer counters.
+                            index = readed = 0;                                                                                           // Set them to 0.
+
+                            while (readed != file.Length) {                                                                               // Loop over all bytes now and convert.
+                                toStringify = br.ReadBytes(toStringify.Length);                                                           // Read 4 bytes.
+
+                                if (args[1] == "-2") newFile.WriteLine(newCrap, EndianSwapp(BitConverter.ToString(BitConverter.GetBytes(index * 4)).Replace("-", "")), BitConverter.ToString(toStringify).Replace("-", ""));   // Stringify the bytes, format and write it into the file.
+                                else if (args[1] == "-3") {
+                                    string trim = BitConverter.ToString(toStringify).Replace("-", "").TrimStart(new char[] { '0' }) + ",";
+                                    if (trim.Length == 1) trim = "0" + trim;
+                                    newFile.Write(trim);
+                                }
+                                else if (args[1] == "-1") newFile.WriteLine(u32, index.ToString(), BitConverter.ToString(toStringify).Replace("-", "")); // Stringify the bytes, format and write it into the file.
+
+                                index++;                                                                                                  // Count indexer up.
+                                readed += toStringify.Length;                                                                             // Count readed bytes up.
+                            }
+                        } // Write the closing clamb for the function.
+                        if (args[1] == "-2" || args[1] == "-1") trick = encode.GetBytes("}\n");                                           // Encode the function closing clamb.
+                        else trick = encode.GetBytes("]\n");                                                                              // Encode the function closing clamb.
+                        trick.Write(newFile);
+                    }
                 } catch (Exception e) { throw new Exception(e.ToString()); }
             } else Console.WriteLine("Can not access the file !");                                                                    // Error.
             Console.WriteLine("Done!\nThx for using my Tool ! :)\nPress any key to continue...");
